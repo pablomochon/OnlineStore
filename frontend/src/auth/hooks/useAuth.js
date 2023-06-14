39e40063af74991d0169497af103b837ff330c66
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-06-14 21:43:27 +0200

    add frontend first version

---
+++ frontend/src/auth/hooks/useAuth.js
@@ -0,0 +1,46 @@
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { loginReducer } from "../reducers/loginReducer";
+import { loginUser } from "../services/authService";
+
+const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
+    isAuth: false,
+    user: undefined,
+}
+export const useAuth = () => {
+
+    const [login, dispatch] = useReducer(loginReducer, initialLogin);
+    const navigate = useNavigate();
+
+    const handlerLogin = ({ username, password }) => {
+        const isLogin = loginUser({ username, password });
+        
+        if (isLogin) {
+            const user = { username: 'admin' }
+            dispatch({
+                type: 'login',
+                payload: user,
+            });
+            sessionStorage.setItem('login', JSON.stringify({
+                isAuth: true,
+                user,
+            }));
+            navigate('/users');
+        } else {
+            Swal.fire('Error Login', 'Username o password invalidos', 'error');
+        }
+    }
+
+    const handlerLogout = () => {
+        dispatch({
+            type: 'logout',
+        });
+        sessionStorage.removeItem('login');
+    }
+    return {
+        login,
+        handlerLogin,
+        handlerLogout,
+    }
+}
