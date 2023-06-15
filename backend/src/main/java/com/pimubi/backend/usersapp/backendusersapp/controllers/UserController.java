3b2f38c3e5046e6746fc68b5b5db85a70a106d86
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-06-15 18:27:52 +0200

    Validating the controller POST and PUT methods

--- backend/src/main/java/com/pimubi/backend/usersapp/backendusersapp/controllers/UserController.java
+++ backend/src/main/java/com/pimubi/backend/usersapp/backendusersapp/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.pimubi.backend.usersapp.backendusersapp.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pimubi.backend.usersapp.backendusersapp.models.entities.User;
 import com.pimubi.backend.usersapp.backendusersapp.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -43,12 +48,18 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody User user){
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+        if (result.hasErrors()) {
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody User user, @PathVariable Long id) {
+    public ResponseEntity<?> update(@Valid @RequestBody User user,BindingResult result, @PathVariable Long id) {
+        if (result.hasErrors()) {
+            return validation(result);
+        }
         Optional<User> o = service.update(user, id);
         if(o.isPresent()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(o.orElseThrow());
@@ -65,4 +76,16 @@ public class UserController {
         }
         return ResponseEntity.notFound().build();
     }
+
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors  = new HashMap<>();
+
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
