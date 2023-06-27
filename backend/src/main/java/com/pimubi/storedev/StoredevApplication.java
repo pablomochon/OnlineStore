3c10d910464ae99bc2d9a53b5ba2a428cf52a8f0
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-06-27 17:50:53 +0200

    Add User and Role repositories

---
+++ backend/src/main/java/com/pimubi/storedev/StoredevApplication.java
@@ -0,0 +1,13 @@
+package com.pimubi.storedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoredevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoredevApplication.class, args);
+	}
+
+}
