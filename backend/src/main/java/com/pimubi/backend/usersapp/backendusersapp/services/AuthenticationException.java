e794c89876b7768d4d37eaa7ffe92176decd9021
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-06-27 13:40:29 +0200

    New spring boot backend

--- backend/src/main/java/com/pimubi/backend/usersapp/backendusersapp/services/AuthenticationException.java
+++
@@ -1,6 +0,0 @@
-package com.pimubi.backend.usersapp.backendusersapp.services;
-
-public class AuthenticationException extends Throwable {
-    public AuthenticationException(String invalidUsernameOrPassword) {
-    }
-}
