0627faab877235995bdadd38c8bc88931ac9776d
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-06-27 17:53:40 +0200

    Fix test

---
+++ backend/src/test/java/com/pimubi/storedev/StoredevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimubi.storedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoredevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
