90a61f6052662115cb16637f6064e62b6f20ae0a
Pablo Mochón Ubiña <pimubiweb@gmail.com>
2023-07-28 17:17:11 +0200

    Add UserService

---
+++ backend/src/main/java/com/pimubi/storedev/services/UserService.java
@@ -0,0 +1,35 @@
+package com.pimubi.storedev.services;
+
+import com.pimubi.storedev.models.User;
+import com.pimubi.storedev.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
